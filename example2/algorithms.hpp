aa0238df73270035e3ee4e47a3682fd5239afeb6
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2021-06-23 12:30:42 +0500

    Feed handler based example

--- example2/algorithms.hpp
+++ example2/algorithms.hpp
@@ -7,10 +7,16 @@ namespace app
     namespace algorithms
     {
 
-        class algo1 : public feed_handler::book_delegate
+        class algo1 : public feed_handler::book_delegate, public feed_handler::event_delegate
         {
-            void on_book_updated(void) override
+            void on_book_updated(feed_handler::book *book) override
             {
+                std::cout << "book updated" << std::endl;
+            }
+
+            void on_event_received() override
+            {
+                std::cout << "event received" << std::endl;
             }
         };
     }
