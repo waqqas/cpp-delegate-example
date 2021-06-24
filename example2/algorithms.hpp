0360ea82c80500fbec1662c0b863f5e0a9b59a6d
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2021-06-24 06:23:43 +0500

    Calling delegates from two threads

--- example2/algorithms.hpp
+++ example2/algorithms.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "book_delegate.hpp"
+#include <cstdint>
+#include <iostream>
 
 namespace app
 {
@@ -10,6 +12,10 @@ namespace app
 
         class algo1 : public book<algo1>::book_delegate, public book<algo1>::event_delegate
         {
+        private:
+            uint32_t _book_update_count = {0};
+            uint32_t _event_count = {0};
+
         public:
             using pointer = std::shared_ptr<algo1>;
 
@@ -20,12 +26,20 @@ namespace app
 
             void on_book_updated(book<algo1>::pointer book)
             {
-                std::cout << "book updated" << std::endl;
+                _book_update_count++;
+                // std::cout << "book update count: " << _book_update_count << std::endl;
             }
 
             void on_event_received()
             {
-                std::cout << "event received" << std::endl;
+                _event_count++;
+                // std::cout << "event count: " << _event_count << std::endl;
+            }
+
+            void print_stats()
+            {
+                std::cout << "book update count: " << _book_update_count << std::endl;
+                std::cout << "event count: " << _event_count << std::endl;
             }
         };
     }
--- example2/main.cpp
+++ example2/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-
 #include "algorithms.hpp"
+
+#include <iostream>
+#include <thread>
+#include <cstdint>
 
 int main(int argc, char *argv[])
 {
@@ -12,11 +14,37 @@ int main(int argc, char *argv[])
   book1->bookDelegate = algo;
   book1->eventDelegate = algo;
 
-  feed_handler::packet<feed_handler::packet_type::book_update> pkt1;
-  book1->process_packet(pkt1);
+  feed_handler::book<current_algo>::pointer book2 = feed_handler::book<current_algo>::make_pointer();
+  book2->bookDelegate = algo;
+  book2->eventDelegate = algo;
 
-  feed_handler::packet<feed_handler::packet_type::event> pkt2;
-  book1->process_packet(pkt2);
+  std::thread t1{[&]()
+                 {
+                   for (uint32_t loop = 0; loop < 100; loop++)
+                   {
+                     feed_handler::packet<feed_handler::packet_type::book_update> pkt1;
+                     book1->process_packet(pkt1);
+
+                     feed_handler::packet<feed_handler::packet_type::event> pkt2;
+                     book1->process_packet(pkt2);
+                   }
+                 }};
+
+  std::thread t2{[&]()
+                 {
+                   for (uint32_t loop = 0; loop < 100; loop++)
+                   {
+                     feed_handler::packet<feed_handler::packet_type::book_update> pkt1;
+                     book2->process_packet(pkt1);
+
+                     feed_handler::packet<feed_handler::packet_type::event> pkt2;
+                     book2->process_packet(pkt2);
+                   }
+                 }};
+  t1.join();
+  t2.join();
+
+  algo->print_stats();
 
   return 0;
 }
