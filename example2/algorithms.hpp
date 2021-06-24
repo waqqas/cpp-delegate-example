9c863abf8214c364ef62041720500ee2a6255f76
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2021-06-24 11:21:12 +0500

    Synchronising delegates (#4)
    * Calling delegates from two threads
    
    * Using mutex to fix concurrency problem
    
    * Using boost::asio to synchronize delegates

--- example2/algorithms.hpp
+++ example2/algorithms.hpp
@@ -2,30 +2,46 @@
 
 #include "book_delegate.hpp"
 
+#include <boost/asio.hpp>
+#include <cstdint>
+#include <iostream>
+// #include <mutex>
+
 namespace app
 {
     namespace algorithms
     {
         using namespace feed_handler;
+        using namespace boost::asio;
 
         class algo1 : public book<algo1>::book_delegate, public book<algo1>::event_delegate
         {
+        private:
+            uint32_t _book_update_count = {0};
+            uint32_t _event_count = {0};
+            // std::mutex _mutex;
+
         public:
             using pointer = std::shared_ptr<algo1>;
 
-            static pointer make_pointer()
-            {
-                return std::make_shared<algo1>();
-            }
+            algo1(io_context &io) : book<algo1>::book_delegate(io), book<algo1>::event_delegate(io) {}
 
             void on_book_updated(book<algo1>::pointer book)
             {
-                std::cout << "book updated" << std::endl;
+                // std::lock_guard<std::mutex> lock(_mutex);
+                _book_update_count++;
             }
 
             void on_event_received()
             {
-                std::cout << "event received" << std::endl;
+                // std::lock_guard<std::mutex> lock(_mutex);
+                _event_count++;
+            }
+
+            void print_stats()
+            {
+                std::cout << "book update count : " << _book_update_count << std::endl;
+                std::cout << "event count       : " << _event_count << std::endl;
             }
         };
     }
--- example2/main.cpp
+++ example2/main.cpp
@@ -1,22 +1,59 @@
-#include <iostream>
-
 #include "algorithms.hpp"
+
+#include <iostream>
+#include <thread>
+#include <cstdint>
+#include <boost/asio.hpp>
 
 int main(int argc, char *argv[])
 {
   using current_algo = app::algorithms::algo1;
+  using namespace boost::asio;
 
-  current_algo::pointer algo = current_algo::make_pointer();
+  io_context io;
+
+  current_algo::pointer algo = std::make_shared<current_algo>(io);
 
   feed_handler::book<current_algo>::pointer book1 = feed_handler::book<current_algo>::make_pointer();
   book1->bookDelegate = algo;
   book1->eventDelegate = algo;
 
-  feed_handler::packet<feed_handler::packet_type::book_update> pkt1;
-  book1->process_packet(pkt1);
+  feed_handler::book<current_algo>::pointer book2 = feed_handler::book<current_algo>::make_pointer();
+  book2->bookDelegate = algo;
+  book2->eventDelegate = algo;
 
-  feed_handler::packet<feed_handler::packet_type::event> pkt2;
-  book1->process_packet(pkt2);
+  std::thread book1thread{
+      [&]()
+      {
+        for (uint32_t loop = 0; loop < 1000; loop++)
+        {
+          feed_handler::packet<feed_handler::packet_type::book_update> pkt1;
+          book1->process_packet(pkt1);
+
+          feed_handler::packet<feed_handler::packet_type::event> pkt2;
+          book1->process_packet(pkt2);
+        }
+      }};
+
+  std::thread book2thread{
+      [&]()
+      {
+        for (uint32_t loop = 0; loop < 1000; loop++)
+        {
+          feed_handler::packet<feed_handler::packet_type::book_update> pkt1;
+          book2->process_packet(pkt1);
+
+          feed_handler::packet<feed_handler::packet_type::event> pkt2;
+          book2->process_packet(pkt2);
+        }
+      }};
+
+  book1thread.join();
+  book2thread.join();
+
+  io.run();
+
+  algo->print_stats();
 
   return 0;
 }
